b3e55192b50865c54009a9cafa683fed903a20a5
Eric <ep310273@gmail.com>
2021-11-16 18:51:19 -0800

    Update emulator.js

--- index/data/emulator.js
+++ index/data/emulator.js
@@ -18141,8 +18141,8 @@ var EJS = function(_0x574f5e) {
                                 var _0x14264d = _0x428003('li', {});
                                 _0x14264d['appendChild'](_0x428003('a', {
                                     'target': '_blank',
-                                    'href': 'https://www.emulatorjs.com'
-                                }, 'EmulatorJS v' ['concat'](this['version']))), _0x2a1dda['appendChild'](_0x14264d), _0x2a1dda['setAttribute']('menu', '');
+                                    'href': 'https://thedoughnutman.github.io'
+                                }, 'Emulator v' ['concat'](this['version']))), _0x2a1dda['appendChild'](_0x14264d), _0x2a1dda['setAttribute']('menu', '');
                             }
                             _0x2c1832 && clearTimeout(_0x2c1832), _0x2c1832 = setTimeout(function() {}, 0xfa0), _0x7f9f36['contextMenu']['timer'] = _0x2c1832;
                         } else _0x7f9f36['contextMenu']['style']['display'] = 'none';
